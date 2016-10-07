20530852cae590fdea690f26738cc2b0c3afd25c
pablocandela <pabloantonio@ciencias.unam.mx>
2016-10-06 19:41:58 -0600

    Programa para abrir interfaz del cliente

---
+++ cliente.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt4 import QtGui,QtCore, uic
+
+class Ventana(QtGui.QMainWindow):
+    def __init__(self):
+        super(Ventana, self).__init__()
+        uic.loadUi('cliente.ui', self)
+        header_horizontal = self.tableWidget.horizontalHeader()
+        header_horizontal.setResizeMode(QtGui.QHeaderView.Stretch)
+        header_vertical = self.tableWidget.verticalHeader()
+        header_vertical.setResizeMode(QtGui.QHeaderView.Stretch)
+        self.show()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ventana = Ventana()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
